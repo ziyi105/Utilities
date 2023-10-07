f0e786e2396a9f4304a82edc836231826be8d3e1
ziyi105 <ahziyi@gmail.com>
2023-10-07 18:34:59 +0800

    Split string to parts
    Fixes #3

--- src/main/java/org/utils/stringutils/StringParser.java
+++ src/main/java/org/utils/stringutils/StringParser.java
@@ -1,5 +1,7 @@
 package org.utils.stringutils;
 
+import org.utils.exceptions.InvalidStringInputException;
+
 import java.util.ArrayList;
 import java.util.Arrays;
 
@@ -12,8 +14,11 @@ public class StringParser {
      * @param separator    a string representing the separator that you want to split the string on
      * @return ArrayList of strings
      */
-    public ArrayList<String> splitString(String stringToSplit, String separator) {
+    public ArrayList<String> splitString(String stringToSplit, String separator) throws InvalidStringInputException {
 
+        if (stringToSplit == null) {
+            throw new InvalidStringInputException();
+        }
         ArrayList<String> stringParts = new ArrayList<>();
         stringParts.addAll(Arrays.asList(stringToSplit.split(separator)));
 
